4a5532f9910fde9b0cf87e96ecc6b590da72f594
raphaelrpl <raphael.wcosta@gmail.com>
2019-12-09 11:54:47 -0300

    Add utility to save record

--- bdc_db/models/base_sql.py
+++ bdc_db/models/base_sql.py
@@ -11,3 +11,17 @@ class BaseModel(db.Model):
     @classmethod
     def query(cls) -> Query:
         return db.session.query(cls)
+
+    def save(self, commit=True):
+        """
+        Save record in database
+
+        Args:
+            commit (bool) - Auto commit. Default is True
+        """
+
+        with db.session.begin_nested():
+            db.session.add(self)
+
+        if commit:
+            db.session.commit()

78de768d9f5e8a7559573b8567f23c494e06d805
raphaelrpl <raphael.wcosta@gmail.com>
2020-02-20 11:14:33 -0300

    Add fixtures command and changes on tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,123 +8,44 @@
 
 """Config test fixtures."""
 
-from json import loads as json_parser
 from typing import List
-from bdc_db.models import db, Asset, Band, Collection, CollectionItem, CollectionTile, CompositeFunctionSchema, GrsSchema, Tile
+from bdc_db.models import db, Collection, Tile
 from flask import Flask
-from pkg_resources import resource_string
 import pytest
 
 from bdc_db.cli import create_app
-
-
-def load_model(fixture_path: str, model_class):
-    """Load fixture to database.
-
-    Args:
-        fixture_path - Path relative to fixtures. i.e 'data/tiles.json'
-        model_class - SQLAlchemy Model Class
-    """
-    schemas = json_parser(resource_string(__name__, fixture_path))
-
-    with db.session.begin_nested():
-        for schema in schemas:
-            model = model_class(**schema)
-
-            model.save(commit=False)
-
-
-def load_collections(fixture_path: str):
-    """Load default collections to database.
-
-    Args:
-        fixture_path - Path relative to fixtures. i.e 'data/tiles.json'
-    """
-    collections = json_parser(resource_string(__name__, fixture_path))
-
-    with db.session.begin_nested():
-        for collection in collections:
-            bands = collection.pop('bands')
-
-            c = Collection(**collection)
-            c.save(commit=False)
-
-            for band in bands:
-                b = Band(**band)
-                b.collection = c
-
-                b.save(commit=False)
-
-
-def load_items(fixture_path: str):
-    """Load default items and assets to database.
-
-    Args:
-        fixture_path - Path relative to fixtures. i.e 'data/items.json'
-    """
-    items = json_parser(resource_string(__name__, fixture_path))
-
-    with db.session.begin_nested():
-        for item in items:
-            assets = item.pop('assets')
-
-            c_item = CollectionItem(**item)
-            c_item.save(commit=False)
-
-            c_tile = CollectionTile()
-            c_tile.collection_id = c_item.collection_id
-            c_tile.grs_schema_id = c_item.grs_schema_id
-            c_tile.tile_id = c_item.tile_id
-            c_tile.save(commit=False)
-
-            for asset in assets:
-                b = Band.query().filter(
-                    Band.collection_id == c_item.collection_id,
-                    Band.name == asset.pop('band')
-                ).one()
-
-                a = Asset(**asset)
-                a.band = b
-
-                a.save(commit=False)
-
-
-def load_fixtures():
-    """Load default database fixtures."""
-    load_model('data/grs_schemas.json', GrsSchema)
-    load_model('data/tiles.json', Tile)
-    load_model('data/composite_functions.json', CompositeFunctionSchema)
-    load_collections('data/collections.json')
-    load_items('data/items.json')
-
-    db.session.commit()
+from bdc_db.fixtures.cli import load_fixtures
 
 
 @pytest.fixture
 def app() -> Flask:
-    """Create flask app context."""
+    """Create flask app and set app_context."""
     _app = create_app()
 
     with _app.app_context():
         db.drop_all()
         db.create_all()
 
-        # Initialize Fixtures
-        load_fixtures()
-
-        yield _app
+        yield app
 
         db.session.close_all()
         db.drop_all()
 
 
 @pytest.fixture
-def tiles(app: Flask) -> List[Tile]:
+def db_context(app: Flask):
+    """Create database context to load fixtures."""
+    load_fixtures()
+    yield app
+
+
+@pytest.fixture
+def tiles(db_context) -> List[Tile]:
     """Retrieve all collections on database loaded from fixtures."""
     return Tile.query().filter().all()
 
 
 @pytest.fixture
-def collections(app: Flask) -> List[Collection]:
+def collections(db_context) -> List[Collection]:
     """Retrieve all collections on database loaded from fixtures."""
     return Collection.query().filter().all()

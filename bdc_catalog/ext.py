9d653439a8ef22939699a0306e02226027b3d2ba
gqueiroz <gribeiro.mg@gmail.com>
2020-08-17 17:52:17 -0300

    Review with pydocstyle

--- bdc_catalog/ext.py
+++ bdc_catalog/ext.py
@@ -6,16 +6,34 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
+"""Image catalog extension for Brazil Data Cube applications and services."""
+
 from flask import Flask
 
 from .cli import cli
 
 
 class BDCCatalog:
+    """Image catalog extension."""
+
     def __init__(self, app=None, **kwargs):
+        """Initialize the catalog extension.
+
+        Args:
+            app: Flask application
+            kwargs: Optional arguments (not used).
+        """
         if app:
             self.init_app(app, **kwargs)
 
+
     def init_app(self, app: Flask, **kwargs):
+        """Initialize Flask application instance.
+
+        Args:
+            app: Flask application
+            kwargs: Optional arguments (not used).
+        """
         app.extensions['bdc-catalog'] = self
+
         app.cli.add_command(cli)

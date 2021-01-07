e420e6cb3befbadad6e86002a7d99e08f69df29c
raphaelrpl <raphael.wcosta@gmail.com>
2021-01-07 17:32:48 -0300

    close #134 - generate check_sum from iostream object

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,7 @@
 
 """Unit-test for check_sum multihash generation."""
 
+from io import BytesIO
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -20,12 +21,19 @@ def test_check_sum_sha256():
     """Test basic check_sum multihash generation."""
     with TemporaryDirectory() as tmp:
         tmp_file = Path(tmp) / 'file.txt'
+        message = 'Testing'
         with open(tmp_file, 'w') as f:
-            f.write('Testing')
+            f.write(message)
 
         _hash = multihash_checksum_sha256(tmp_file)
 
-        assert _hash == '1220e806a291cfc3e61f83b98d344ee57e3e8933cccece4fb45e1481f1f560e70eb1'
+        expected_hash = '1220e806a291cfc3e61f83b98d344ee57e3e8933cccece4fb45e1481f1f560e70eb1'
+
+        assert _hash == expected_hash
+        # Try using stream instead file
+        stream = BytesIO(message.encode())
+        stream_hash = multihash_checksum_sha256(stream)
+        assert stream_hash == expected_hash
 
         digest = multihash.from_hex_string(_hash)
 

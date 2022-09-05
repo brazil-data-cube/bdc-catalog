a3bbf95b389c2fc1ad856c81e847f94f1f1868d0
raphael.wcosta@gmail.com <raphael.wcosta@gmail.com>
2022-09-05 18:34:59 -0300

    :bookmark: Set version alpha3

--- bdc_catalog/models/role.py
+++ bdc_catalog/models/role.py
@@ -1,6 +1,6 @@
 #
 # This file is part of Brazil Data Cube Database module.
-# Copyright (C) 2019 INPE.
+# Copyright (C) 2022 INPE.
 #
 # Brazil Data Cube Database module is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.

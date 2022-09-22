ad9ec130671070236438d4c15f38d82ef8b22455
raphael.wcosta@gmail.com <raphael.wcosta@gmail.com>
2022-09-22 10:14:39 -0300

    :fire: Change license to GPL v3

--- examples/app.py
+++ examples/app.py
@@ -1,17 +1,31 @@
 #
 # This file is part of BDC-Catalog.
-# Copyright (C) 2019-2020 INPE.
+# Copyright (C) 2022 INPE.
 #
-# BDC-Catalog is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
 
 from flask import Flask
 
 from bdc_catalog import BDCCatalog
+from bdc_catalog.models import Collection, Item
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/bdc_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/bdc_catalog'
 
 BDCCatalog(app)
+
+with app.app_context():
+    # Retrieve all collections (Available Only)
+    collections = Collection.query().filter(Collection.is_available.is_(True)).all()
